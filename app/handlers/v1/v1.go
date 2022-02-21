659aede7bbfd77aec6c1b6c9bb1b76a2bf71e226
frisk038 <frisktrentehuit@gmail.com>
2022-02-21 22:34:44 +0100

    returns a json + add route /combination

--- app/handlers/v1/v1.go
+++ app/handlers/v1/v1.go
@@ -4,17 +4,27 @@ package v1
 import (
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/frisk038/gocolormind/business/generate"
 	"github.com/gin-gonic/gin"
 )
 
+type combination struct {
+	Date        time.Time `json:"date"`
+	Combination []string  `json:"combi"`
+}
+
 func API(build string, log *log.Logger) *gin.Engine {
 	router := gin.New()
 	router.Use(gin.Logger())
+	gin.SetMode(gin.ReleaseMode)
 
-	router.GET("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, generate.Generate())
+	router.GET("/combination", func(c *gin.Context) {
+		c.JSON(http.StatusOK, combination{
+			Date:        time.Now(),
+			Combination: generate.Generate(),
+		})
 	})
 
 	return router
